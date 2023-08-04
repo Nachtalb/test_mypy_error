98ffd1e6ea97ac7e57e1a6eeefa1560d3ed3b4be
Nachtalb <na@nachtalb.io>
2023-08-04 19:02:17 +0200

    Init reflex

---
+++ rxconfig.py
@@ -0,0 +1,10 @@
+import reflex as rx
+
+class TestmypyerrorConfig(rx.Config):
+    pass
+
+config = TestmypyerrorConfig(
+    app_name="test_mypy_error",
+    db_url="sqlite:///reflex.db",
+    env=rx.Env.DEV,
+)
